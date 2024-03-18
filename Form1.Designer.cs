1a45169545473b6849137116f22d5598fc62b69a
Outerland1145 <162087937+Outerland1145@users.noreply.github.com>
2024-03-18 14:31:11 +0800

    加入工具介面和主題文字

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,13 +43,14 @@ namespace NEW_Programming
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("新細明體", 36F);
-            this.label1.Location = new System.Drawing.Point(54, 66);
+            this.label1.Location = new System.Drawing.Point(54, 73);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(116, 48);
             this.label1.TabIndex = 0;
@@ -59,7 +60,7 @@ namespace NEW_Programming
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("新細明體", 36F);
-            this.label2.Location = new System.Drawing.Point(324, 66);
+            this.label2.Location = new System.Drawing.Point(324, 73);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(116, 48);
             this.label2.TabIndex = 1;
@@ -69,7 +70,7 @@ namespace NEW_Programming
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("新細明體", 36F);
-            this.label3.Location = new System.Drawing.Point(595, 66);
+            this.label3.Location = new System.Drawing.Point(595, 73);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(116, 48);
             this.label3.TabIndex = 2;
@@ -79,7 +80,7 @@ namespace NEW_Programming
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("新細明體", 36F);
-            this.label4.Location = new System.Drawing.Point(54, 186);
+            this.label4.Location = new System.Drawing.Point(54, 193);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(116, 48);
             this.label4.TabIndex = 3;
@@ -89,7 +90,7 @@ namespace NEW_Programming
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("新細明體", 36F);
-            this.label5.Location = new System.Drawing.Point(324, 186);
+            this.label5.Location = new System.Drawing.Point(324, 193);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(116, 48);
             this.label5.TabIndex = 4;
@@ -99,7 +100,7 @@ namespace NEW_Programming
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("新細明體", 36F);
-            this.label6.Location = new System.Drawing.Point(595, 186);
+            this.label6.Location = new System.Drawing.Point(595, 193);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(116, 48);
             this.label6.TabIndex = 5;
@@ -108,7 +109,7 @@ namespace NEW_Programming
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("新細明體", 18F);
-            this.textBox1.Location = new System.Drawing.Point(62, 117);
+            this.textBox1.Location = new System.Drawing.Point(62, 124);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 36);
             this.textBox1.TabIndex = 6;
@@ -116,7 +117,7 @@ namespace NEW_Programming
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("新細明體", 18F);
-            this.textBox2.Location = new System.Drawing.Point(332, 117);
+            this.textBox2.Location = new System.Drawing.Point(332, 124);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 36);
             this.textBox2.TabIndex = 7;
@@ -124,7 +125,7 @@ namespace NEW_Programming
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("新細明體", 18F);
-            this.textBox3.Location = new System.Drawing.Point(603, 117);
+            this.textBox3.Location = new System.Drawing.Point(603, 124);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 36);
             this.textBox3.TabIndex = 8;
@@ -132,7 +133,7 @@ namespace NEW_Programming
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("新細明體", 18F);
-            this.textBox4.Location = new System.Drawing.Point(62, 237);
+            this.textBox4.Location = new System.Drawing.Point(62, 244);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 36);
             this.textBox4.TabIndex = 9;
@@ -140,7 +141,7 @@ namespace NEW_Programming
             // textBox5
             // 
             this.textBox5.Font = new System.Drawing.Font("新細明體", 18F);
-            this.textBox5.Location = new System.Drawing.Point(332, 237);
+            this.textBox5.Location = new System.Drawing.Point(332, 244);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 36);
             this.textBox5.TabIndex = 10;
@@ -148,16 +149,27 @@ namespace NEW_Programming
             // textBox6
             // 
             this.textBox6.Font = new System.Drawing.Font("新細明體", 18F);
-            this.textBox6.Location = new System.Drawing.Point(603, 237);
+            this.textBox6.Location = new System.Drawing.Point(603, 244);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(100, 36);
             this.textBox6.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("新細明體", 24F);
+            this.label7.Location = new System.Drawing.Point(56, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(207, 32);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "單位轉換工具";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
@@ -192,6 +204,7 @@ namespace NEW_Programming
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
