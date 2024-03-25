4a3fa0dd9a6a574aefed2f588cfd6f0beb9c69fc
Outerland1145 <162087937+Outerland1145@users.noreply.github.com>
2024-03-25 13:49:14 +0800

    完成英公制單位換算機制

--- Form1.cs
+++ Form1.cs
@@ -37,6 +37,10 @@ namespace NEW_Programming
 
             TXKM.Text = string.Format("{0:0.##########}", douM / 1000);
             TXCM.Text = string.Format("{0:0.##########}", douM*100);
+
+            TXinch.Text = string.Format("{0:0.##########}", douM*100 / 2.54);
+            TXFT.Text = string.Format("{0:0.##########}", douM*100 / 30.48);
+            TXyard.Text = string.Format("{0:0.##########}", douM*100 / 91.44);
         }
 
         private void TXKM_KeyUp(object sender, KeyEventArgs e)
@@ -47,6 +51,10 @@ namespace NEW_Programming
 
             TXM.Text = string.Format("{0:0.##########}", douKM*1000);
             TXCM.Text = string.Format("{0:0.##########}", douKM*100000);
+
+            TXinch.Text = string.Format("{0:0.##########}", douKM * 100000 / 2.54);
+            TXFT.Text = string.Format("{0:0.##########}", douKM * 100000 / 30.48);
+            TXyard.Text = string.Format("{0:0.##########}", douKM * 100000 / 91.44);
         }
 
         private void TXinch_KeyUp(object sender, KeyEventArgs e)
@@ -57,6 +65,9 @@ namespace NEW_Programming
 
             TXFT.Text = string.Format("{0:0.##########}", douIN/12);
             TXyard.Text = string.Format("{0:0.##########}", douIN/36);
+            TXCM.Text = string.Format("{0:0.##########}", douIN * 2.54);
+            TXM.Text = string.Format("{0:0.##########}", douIN * 2.54 * 0.01);
+            TXKM.Text = string.Format("{0:0.##########}", douIN * 2.54 * 0.01 * 0.001);
 
         }
 
@@ -68,6 +79,9 @@ namespace NEW_Programming
 
             TXinch.Text = string.Format("{0:0.##########}", douFT * 12);
             TXyard.Text = string.Format("{0:0.##########}", douFT/3);
+            TXCM.Text = string.Format("{0:0.##########}", douFT * 30.48);
+            TXM.Text = string.Format("{0:0.##########}", douFT * 30.48 * 0.01);
+            TXKM.Text = string.Format("{0:0.##########}", douFT * 30.48 * 0.01 * 0.001);
 
         }
 
@@ -79,6 +93,9 @@ namespace NEW_Programming
 
             TXinch.Text = string.Format("{0:0.##########}", douYard * 36);
             TXFT.Text = string.Format("{0:0.##########}", douYard * 3);
+            TXCM.Text = string.Format("{0:0.##########}", douYard * 91.44);
+            TXM.Text = string.Format("{0:0.##########}", douYard * 91.44*0.01);
+            TXKM.Text = string.Format("{0:0.##########}", douYard * 91.44 * 0.01*0.001);
         }
     }
 }
